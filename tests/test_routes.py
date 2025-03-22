8f2d4a76eaf9c9352d7fc3550c8c7c3ea1cca617
rudermanmb1 <rudermanb1@gmail.com>
2025-03-22 19:02:58 -0400

    added build task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -26,6 +26,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -203,4 +204,3 @@ class TestAccountService(TestCase):
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-    
